c1bca5a79064550969361295d9df1ac7363c4b4f
Михаил Мокрушин <mokrushinmix@yandex.ru>
2025-02-18 23:24:29 +0500

    правки

--- strawberry/js/script.js
+++ strawberry/js/script.js
@@ -15,8 +15,6 @@ document.getElementById('list1').addEventListener('change', function () {
 	document.getElementById('sum1').value = selectedValue
 });
 
-
-
 document.getElementById('list2').addEventListener('change', function () {
 	var selectedValue = this.value
 
@@ -24,18 +22,46 @@ document.getElementById('list2').addEventListener('change', function () {
 	document.getElementById('sum2').value = selectedValue
 });
 
-
 document.getElementById('list3').addEventListener('change', function () {
 	var selectedValue = this.value
 
 	// Обновляем сумму
 	document.getElementById('sum3').value = selectedValue
 });
+
 document.getElementById('list4').addEventListener('change', function () {
 	var selectedValue = this.value
 
 	// Обновляем сумму
 	document.getElementById('sum4').value = selectedValue
+});
+
+document.getElementById('list5').addEventListener('change', function () {
+	var selectedValue = this.value
+
+	// Обновляем сумму
+	document.getElementById('sum5').value = selectedValue
+});
+
+document.getElementById('list6').addEventListener('change', function () {
+	var selectedValue = this.value
+
+	// Обновляем сумму
+	document.getElementById('sum6').value = selectedValue
+});
+
+document.getElementById('list7').addEventListener('change', function () {
+	var selectedValue = this.value
+
+	// Обновляем сумму
+	document.getElementById('sum7').value = selectedValue
+});
+
+document.getElementById('list8').addEventListener('change', function () {
+	var selectedValue = this.value
+
+	// Обновляем сумму
+	document.getElementById('sum8').value = selectedValue
 });
 
 
