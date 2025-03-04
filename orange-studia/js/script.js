4ab0c3d518e9f94b9cecccc4fee4101fff1b3812
Михаил Мокрушин <mokrushinmix@yandex.ru>
2025-03-04 20:44:00 +0500

    04.03.2025

---
+++ orange-studia/js/script.js
@@ -0,0 +1,8 @@
+const currentDate = new Date()
+
+// Форматируем дату в нужный формат
+const options = { year: 'numeric', month: 'long', day: 'numeric' }
+document.getElementById('currentDate').innerText = currentDate.toLocaleString(
+	'ru-RU',
+	options
+)
