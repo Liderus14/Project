6e4571466696889c0707d8389e1c25b8e2e06776
Nastya <masalskaya198@yandex.ru>
2025-03-24 17:04:56 +0500

    erse3

--- doma/js/popup-gallery.js
+++ doma/js/popup-gallery.js
@@ -1,3 +1,5 @@
+// Лавр
+
 document.getElementById('open-modal3').addEventListener('click', openModal3)
 document.getElementById('modal3').addEventListener('click', closeModal3)
 
@@ -15,4 +17,25 @@ document
 	.querySelector('.close3')
 	.addEventListener('click', function () {
 		document.querySelector('.modal3').style.display = 'none'
+	});
+
+	// Лазурит
+
+	document.getElementById('open-modal4').addEventListener('click', openModal4)
+document.getElementById('modal4').addEventListener('click', closeModal4)
+
+function openModal4() {
+	document.getElementById('modal4').style.display = 'block'
+}
+
+function closeModal4(event) {
+	if (event.target === document.getElementById('modal4')) {
+		document.getElementById('modal4').style.display = 'none'
+	}
+};
+
+document
+	.querySelector('.close4')
+	.addEventListener('click', function () {
+		document.querySelector('.modal4').style.display = 'none'
 	});
